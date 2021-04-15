0ea30629aaed01e4933fa46dc2c5b7626c056b28
Maryam ElOraby <maryam.eloraby@gmail.com>
2021-04-15 23:47:29 +0200

    Initial

---
+++ src/main/java/DBApp.java
@@ -0,0 +1,39 @@
+import java.util.Hashtable;
+import java.util.Iterator;
+
+public class DBApp implements DBAppInterface{
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void createTable(String tableName, String clusteringKey, Hashtable<String, String> colNameType, Hashtable<String, String> colNameMin, Hashtable<String, String> colNameMax) throws DBAppException {
+
+    }
+
+    @Override
+    public void createIndex(String tableName, String[] columnNames) throws DBAppException {
+
+    }
+
+    @Override
+    public void insertIntoTable(String tableName, Hashtable<String, Object> colNameValue) throws DBAppException {
+
+    }
+
+    @Override
+    public void updateTable(String tableName, String clusteringKeyValue, Hashtable<String, Object> columnNameValue) throws DBAppException {
+
+    }
+
+    @Override
+    public void deleteFromTable(String tableName, Hashtable<String, Object> columnNameValue) throws DBAppException {
+
+    }
+
+    @Override
+    public Iterator selectFromTable(SQLTerm[] sqlTerms, String[] arrayOperators) throws DBAppException {
+        return null;
+    }
+}
