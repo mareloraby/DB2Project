df3960e2463b305bfd8247f69e2b4377396ef357
RawanReda <rawan.reda.academia@gmail.com>
2021-04-22 14:48:24 +0200

    push insertIntoTable

--- src/main/java/Page.java
+++ src/main/java/Page.java
@@ -4,17 +4,22 @@ import java.util.Vector;
 
 public class Page implements java.io.Serializable  {
     private Vector<Hashtable> rows;
+    private int min_Pk_Value;
+    private int max_Pk_Value;
+    private int numOfRows;
+
 //    public Vector<Hashtable> page;
 //    public Hashtable row;
 
     public Page(){
-//        this.row = s;
+        min_Pk_Value=0;
+        max_Pk_Value=0;
         rows = new Vector<Hashtable>();
-//        this.page.add(this.row);
+//this.page.add(this.row);
     }
 
     public void addRow(Hashtable s){
-        rows.add(s);
+        numOfRows++; rows.add(s);
     }
     public static void main(String[] args) {
         // write your code here
