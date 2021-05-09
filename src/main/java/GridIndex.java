e71ebb15ce1ddadd1a402a0780310816cdabd909
Maryam <maryam.eloraby@gmail.com>
2021-05-09 20:14:33 +0200

    Some comments

--- src/main/java/GridIndex.java
+++ src/main/java/GridIndex.java
@@ -8,21 +8,21 @@ public class GridIndex implements java.io.Serializable{
     private ArrayList<ArrayList> cols;
 
     GridIndex(String tableName, String [] columnNames){
-
-    //insert into page
-    //dawwar 3al range fel grid index for each col
-    //get to the right cell and check if i have a bucket with this range and insert.
-        // if the bucket is full create an overflow and insert into it
-    //if i don't have a bucket, create a new one
-    for (int i=0; i<columnNames.length; i++){
-        cols.add(new ArrayList<Object>());
-
-    }
+//Array for each dimension
+//*divide range by 10 -> (max of col - min of col)/10 //floor or ceiling?
 
 
+
+//    for (int i=0; i<columnNames.length; i++){
+//        cols.add(new ArrayList<Object>());
+//
+//    }
     }
 
 }
 
-
-//
+//insert into page
+//dawwar 3al range fel grid index for each col
+//get to the right cell and check if i have a bucket with this range and insert.
+// if the bucket is full create an overflow and insert into it
+//if i don't have a bucket, create a new one
